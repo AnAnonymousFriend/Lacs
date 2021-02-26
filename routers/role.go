6bd579765bb58adabd9802758d7422f09219e6fd
kai_wang <871779972@qq.com>
2021-02-26 11:59:46 +0800

    select all

--- routers/role.go
+++ routers/role.go
@@ -11,6 +11,7 @@ func InitRoleCodeRouter(Router *gin.RouterGroup) {
 	{
 		RoleRouter.POST("add", api.AddRole)
 		RoleRouter.GET("findOne", api.FindOne)
-
+		RoleRouter.GET("all", api.FindAll)
+		RoleRouter.POST("deleteRole",api.DeleteRoleById)
 	}
 }
--- server/api/role.go
+++ server/api/role.go
@@ -20,9 +20,9 @@ type role struct {
 // @Produce  json
 // @Success 200 {string} string "{"code":200,"data":{},"msg":"ok"}"
 // @Router /api/v1/login [Get]
-func AddRole(ctx *gin.Context) {
-	g := app.Gin{ctx}
-	roleNameParm := ctx.PostForm("roleName")
+func AddRole(c *gin.Context) {
+	g := app.Gin{c}
+	roleNameParm := c.PostForm("roleName")
 
 	var parm = role{
 		RoleName: roleNameParm,
@@ -43,10 +43,10 @@ func AddRole(ctx *gin.Context) {
 	g.Response(http.StatusOK,e.SUCCESS,e.GetMsg(e.SUCCESS))
 }
 
-func FindOne(ctx *gin.Context)  {
-	g := app.Gin{ctx}
+func FindOne(c *gin.Context)  {
+	g := app.Gin{c}
 	var result role
-	roleNameParm := ctx.Query("roleName")
+	roleNameParm := c.Query("roleName")
 	collection := mogo.NewMongoClient("role")
 	filter := bson.D{{"roleName", roleNameParm}}
 	err := collection.FindOne(context.TODO(), filter).Decode(&result)
@@ -55,4 +55,44 @@ func FindOne(ctx *gin.Context)  {
 	}
 	g.Response(http.StatusOK,e.SUCCESS,result)
 
+}
+
+func FindAll(c *gin.Context)  {
+	g := app.Gin{c}
+	collection := mogo.NewMongoClient("role")
+	var roles []*role
+
+
+	cur, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		panic(err)
+	}
+
+	for cur.Next(context.TODO()) {
+		// create a value into which the single document can be decoded
+		var elem role
+		err := cur.Decode(&elem)
+		if err != nil {
+			println(err)
+		}
+		roles = append(roles, &elem)
+	}
+
+
+
+
+	g.Response(http.StatusOK,e.SUCCESS,roles)
+}
+
+
+func DeleteRoleById(c *gin.Context)  {
+	g := app.Gin{c}
+	roleId := c.Query("roleId")
+	collection := mogo.NewMongoClient("role")
+	filter := bson.D{{"roleId", roleId}}
+	result,err := collection.DeleteOne(context.Background(),filter)
+	if err !=nil {
+		g.Response(http.StatusInternalServerError,e.ERROR,nil)
+	}
+	g.Response(http.StatusOK,e.SUCCESS,result)
 }
