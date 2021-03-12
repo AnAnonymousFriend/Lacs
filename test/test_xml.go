662db2d54499939854c8a95180d0090bf9fee231
kai_wang <871779972@qq.com>
2021-03-12 15:19:36 +0800

    解析XML

--- test/test_xml.go
+++ test/test_xml.go
@@ -10,6 +10,7 @@ import (
 
 type Beans struct {
 	Header CwmpId `xml:"http://schemas.xmlsoap.org/soap/envelope/ Header`
+	Body XmlBody `xml:"http://schemas.xmlsoap.org/soap/envelope/ Body`
 	ListenerContainers []ListenerContainer `xml:"http://www.springframework.org/schema/rabbit listener-container"`
 }
 
@@ -17,6 +18,44 @@ type CwmpId struct {
 	ID string `xml:"urn:dslforum-org:cwmp-1-2 ID"`
 }
 
+type XmlBody struct {
+	Inform DeviceInform `xml:"urn:dslforum-org:cwmp-1-2 Inform"`
+}
+
+type DeviceInform struct {
+	DeviceId     DeviceInfo   `xml:"DeviceId"`
+	Event		Event	`xml:"Event"`
+
+	MaxEnvelopes int `xml:"MaxEnvelopes"`
+	CurrentTime string `xml:"CurrentTime"`
+	RetryCount int `xml:"RetryCount"`
+
+	ParameterList ParameterList `xml:"ParameterList"`
+}
+
+type  ParameterList struct {
+	ParameterValueStruct []ParameterValueStruct `xml:"ParameterValueStruct"`
+}
+
+type ParameterValueStruct struct {
+	Name string `xml:"Name"`
+	Value string `xml:"Value"`
+}
+
+type  Event struct {
+	EventStruct []EventStruct `xml:"EventStruct"`
+}
+
+type  EventStruct struct {
+	EventCode string `xml:"EventCode"`
+}
+
+type DeviceInfo struct {
+	Manufacturer     string   `xml:"Manufacturer"`
+	OUI     		 string   `xml:"OUI"`
+	ProductClass     string   `xml:"ProductClass"`
+	SerialNumber     string   `xml:"SerialNumber"`
+}
 
 
 
@@ -61,11 +100,11 @@ func main() {
 	}
 
 	fmt.Println(v.Header.ID)
-	fmt.Println(len(v.ListenerContainers))
-	//fmt.Println(v.Header)
-	//fmt.Println(v.Envelope)
-	//fmt.Println("SmtpServer : ",v.Header)
-	//fmt.Println("SmtpServer : ",v.HeaderTwo.ID)
-	//fmt.Println("SmtpServer : ",v.XMLName)
+	fmt.Println(v.Body.Inform.DeviceId.Manufacturer)
+	fmt.Println(v.Body.Inform.Event.EventStruct[0].EventCode)
+	fmt.Println(v.Body.Inform.MaxEnvelopes)
+	fmt.Println(v.Body.Inform.CurrentTime)
 
+	fmt.Println(v.Body.Inform.ParameterList.ParameterValueStruct[0].Name)
+	fmt.Println(v.Body.Inform.ParameterList.ParameterValueStruct[0].Value)
 }
